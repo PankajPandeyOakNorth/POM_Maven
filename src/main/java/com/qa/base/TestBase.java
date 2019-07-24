c2016eabe50163309481ca2e5ef84c0c0a34cbd1
Pankaj Pandey <Pankaj.Pandey@OAKNORTH.AI>
2019-07-24 16:54:32 +0530

    test

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -47,8 +47,8 @@ public class TestBase {
 		
 		if(browsername.equals("chrome")){
 			
-//			System.setProperty("webdriver.chrome.driver", "C://Users//pankaj.pandey//Downloads//chromedriver_win32//chromedriver.exe");
-			System.setProperty("webdriver.chrome.driver", "C://ACORN//chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "C://Users//pankaj.pandey//Downloads//chromedriver_win32//chromedriver.exe");
+//			System.setProperty("webdriver.chrome.driver", "C://ACORN//chromedriver.exe");
 			
 			
 			driver = new ChromeDriver();
